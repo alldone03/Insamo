31a6f9fc3c1cb94c208c9a20a20e4f5f795759a3
alldone03 <aldanarsenal@gmail.com>
2026-02-17 23:16:15 +0700

    fixed: login page

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../../lib/auth_context";
-
+import { Mail, Lock, LogIn, Eye, EyeOff, AlertCircle } from "lucide-react";
 
 export default function Login() {
     const [email, setEmail] = useState("");
