ce23c1604d1f7a95bac1374ff067b51876c50cd9
alldone03 <aldanarsenal@gmail.com>
2026-02-25 10:14:09 +0700

    fixed

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -5,8 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     try {
         \Illuminate\Support\Facades\DB::connection()->getPdo();
-        return "200 OK";
+        return "200 OK\n";
     } catch (\Exception $e) {
-        return "500 Error";
+        return "500 Error\n";
     }
 });
